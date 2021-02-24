6ea62638ea9bf6ced715e1f52fd260a59ea8c31e
fahmidaproma <77931794+fahmidaproma@users.noreply.github.com>
2021-02-25 00:43:54 +0600

    lab task

---
+++ Task 3/Problem 4.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>Problem 4</title>
+  </head>
+  <body>
+    <?php
+    $name = "raju";
+    if($name == "raju" || $name == "mina" || $name == "mithu")
+     {
+        echo "<b>hello</b>";
+     }
+
+
+    else echo "you are not recognized";
+
+     ?>
+
+  </body>
+</html>
